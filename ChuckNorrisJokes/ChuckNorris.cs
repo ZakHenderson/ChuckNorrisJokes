ca666279331bae113c948e6b4aa05951db22ce33
Zak <zak.henderson.47@gmail.com>
2023-09-19 16:37:40 -0500

    Add project files.

---
+++ ChuckNorrisJokes/ChuckNorris.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokes
+{
+    public class ChuckNorris
+    {
+
+        public string value { get; set; }
+
+    }
+
+}
+
+
---
+++ ChuckNorrisJokes/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChuckNorrisJokes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string url = @"https://api.chucknorris.io/jokes/categories";
+
+            HttpClient client = new HttpClient();
+
+            string json = client.GetStringAsync(url).Result;
+            string[] categories = JsonConvert.DeserializeObject<string[]>(json);
+            foreach (var item in categories)
+            {
+                cboJokeSelect.Items.Add(item);
+            }
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string url;
+            if (cboJokeSelect.SelectedItem != null)
+            {
+                url = @"https://api.chucknorris.io/jokes/random?category=" + cboJokeSelect.SelectedItem.ToString();
+                HttpClient client = new HttpClient();
+                string json = client.GetStringAsync(url).Result;
+                ChuckNorris joke = JsonConvert.DeserializeObject<ChuckNorris>(json);
+                txtJoke.Text = joke.value;
+            }
+            else
+            {
+                url = @"https://api.chucknorris.io/jokes/random";
+                HttpClient client = new HttpClient();
+                string json = client.GetStringAsync(url).Result;
+                ChuckNorris joke = JsonConvert.DeserializeObject<ChuckNorris>(json);
+                txtJoke.Text = joke.value;
+            }
+
+            
+
+
+        }
+    }
+}
